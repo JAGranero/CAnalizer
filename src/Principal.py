bd748f5bf9d74b2725517b79a12e1e94c5c5cad3
Javier <javierg1992@gmail.com>
2020-11-14 14:17:35 -0300

    Salidas arregladas pero no redireccionadas

--- src/Principal.py
+++ src/Principal.py
@@ -3,35 +3,33 @@ import os
 from antlr4 import *
 from GrammarCLexer import GrammarCLexer
 from GrammarCParser import GrammarCParser
-sys.stdout = open('salida.txt', 'w')
-print('test')
+#sys.stdout = open('OutPut.txt', 'w')
 
 def main(argv):
 
     # Crear para la salida del parser
-    file = open("myOutFile.txt", "w")
-    sys.stdout = open('file', 'w')
+    #file = open("myOutFile.txt", "w")
+    #sys.stdout = open('OutPut.txt', 'w')
 
     # print(argv[1])
 
     for nombre_directorio, dirs, ficheros in os.walk(argv[1]):
-        file.write(nombre_directorio)
-        file.write("\n")
+        print(nombre_directorio)
         for nombre_fichero in ficheros:
-            file.write("   ")
-            file.write(nombre_fichero)
-            file.write("\n")
             if nombre_fichero.endswith(".c"):
+                print("   "+nombre_fichero)
                 try:
                     input_stream = FileStream(nombre_directorio+"\\"+nombre_fichero)
                     lexer = GrammarCLexer(input_stream)
                     stream = CommonTokenStream(lexer)
                     parser = GrammarCParser(stream)
                     tree = parser.compilationUnit()
+                    #print(tree.toStringTree(parser))
+
                 except:
                     print("error en file -> "+nombre_directorio+"\\"+nombre_fichero)
 
-    file.close()
+    #file.close()
 
     # input_stream = FileStream(argv[1])
     # lexer = GrammarCLexer(input_stream)
